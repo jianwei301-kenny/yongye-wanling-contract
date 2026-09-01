bcf18703d45c28cbb298724ee51f380c9de3d776
jianwei301-kenny <jianwei301@gmail.com>
2026-09-01 16:06:04 +0800

    Retire legacy cache-first service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='yongye-wanling-v1';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest','./app-icon.svg']))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+const CACHE='yongye-wanling-v4';
+self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))),self.clients.claim()])));
+self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
 
