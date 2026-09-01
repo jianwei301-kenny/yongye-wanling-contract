78d601552b58b1dc0523aef24f666ac4a2123053
jianwei301-kenny <jianwei301@gmail.com>
2026-09-01 09:51:24 +0800

    Add offline cache

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='yongye-wanling-v1';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest','./app-icon.svg']))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+
